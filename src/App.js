dcd7d98f8fe38866719993fa5b241bd3cd8de2a7
Trzy426 <charleswrogersiii@gmail.com>
2021-08-24 13:08:53 -0700

    completed 3rd card step however footer and header both wont show

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component{
   }
   render(){
     return (
-      <>
+      
         <Router>
           <Header/>
           <Switch>
@@ -37,7 +37,6 @@ class App extends Component{
           </Switch>
           <Footer/>
         </Router>
-      </>
     )
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,89 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react'
+import {createMemoryHistory} from 'history'
+import {Router} from 'react-router-dom'
+import React from 'react' 
+// Imports Enzyme testing and deconstructs Shallow into our test file. 
+import Enzyme, { shallow } from 'enzyme' 
+// Imports Adapter utilizing the latest react version into our test file so we can run a testing render on any component we may need.
+import Adapter from 'enzyme-adapter-react-16' 
+ // Imports in the component we are going to be testing.  
 import App from './App';
+import Home from './pages/Home'
+import CatIndex from './pages/CatIndex'
+import CatShow from './pages/CatShow'
+import CatNew from './pages/CatNew'
+import CatEdit from './pages/CatEdit'
+import { MemoryRouter } from "react-router-dom"
+Enzyme.configure({ adapter: new Adapter() })
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+
+
+describe('app does the rendering', () => {
+  // it('renders header', () => { 
+  //   const renderedApp = shallow(<App/>)
+  //   // console.log(renderedApp.find('Header').debug())
+  //   const renderedHeader = renderedApp.find('Header')
+  //   expect(renderedApp.find('Header').length).toEqual(1);  
+  // })
+  // no work
+  it('it provides a route/ to the home component', () => { 
+    const renderedApp = shallow(<App/>)
+    // console.log(renderedApp.find('Header').debug())
+    const renderedHomeRoute = renderedApp.find('[path="/"]')
+    console.log(renderedHomeRoute.props())
+    expect(renderedHomeRoute.props().component).toEqual(Home);  
+  })
+  it('it provides a route/catindex to the CatIndex component', () => { 
+    const renderedApp = shallow(<App/>)
+    const renderedCatIndexRoute = renderedApp.find('[path="/catindex"]')
+    expect(renderedCatIndexRoute.props().component).toEqual(CatIndex);  
+  })
+  it('it provides a route/catshow/:id to the CatShow component', () => { 
+    const renderedApp = shallow(<App/>)
+    // console.log(renderedApp.find('Header').debug())
+    const renderedCatShowRoute = renderedApp.find('[path="/catshow/:id"]')
+    expect(renderedCatShowRoute.props().component).toEqual(CatShow);  
+  })
+  it('it provides a route/catnew to the CatNew component', () => { 
+    const renderedApp = shallow(<App/>)
+    const renderedCatNewRoute = renderedApp.find('[path="/catnew"]')
+    expect(renderedCatNewRoute.props().component).toEqual(CatNew);  
+  })
+  it('it provides a route/catedit/:id to the CatEdit component', () => { 
+    const renderedApp = shallow(<App/>)
+    const renderedCatEditRoute = renderedApp.find('[path="/catedit/:id"]')
+    expect(renderedCatEditRoute.props().component).toEqual(CatEdit);  
+  })
+  it('it provides a route/notfound to the NotFound component', () => { 
+    const history = createMemoryHistory()
+    history.push("some jibberish")
+    render(
+      <Router history={history}>
+        <App/>
+      </Router>
+    )
+    expect(screen.getByText(/404/i)).toBeInTheDocument();  
+  })
+  
 });
+
+
+
+
+
+
+
+
+
+// describe('Renders header and footer', () => {
+//   it('renders header', () => { 
+//     const renderedApp = shallow(<App/>)
+//     // console.log(renderedApp.find('Header').debug())
+//     const renderedHeader = renderedApp.find('Header')
+
+//     expect(renderedApp.find('Header').length).toEqual(1);  
+//   })
+
+// });
