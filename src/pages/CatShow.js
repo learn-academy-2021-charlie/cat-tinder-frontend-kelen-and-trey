ec280c0f1236971561bc6248b4a8593875af4114
kel-man <kyyafuso@email.com>
2021-08-27 13:50:16 -0700

    connects to rails backend for all previous functions, adds delete functionality, refactors app to use an AppRouter component for routes

--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react'
 import AppContext from '../context/AppContext'
 import { NavLink } from 'react-router-dom'
+import { Button } from '@material-ui/core'
 
-const CatShow = ({classes, match}) => {
+const CatShow = ({history, classes, match}) => {
   const appContext = useContext(AppContext)
   var cat = {}
   if(appContext) cat = appContext.state.cats.find(cat => cat.id === +match.params.id)
+
+  const deleteCat = () => {
+    appContext.handleDeleteCat(cat.id)
+    history.push('/catindex')
+  }
+
   return(
     <>
       <h2>CatShow component</h2>
@@ -13,6 +20,8 @@ const CatShow = ({classes, match}) => {
         <h2>{cat.name} is {cat.age} years old.</h2>
         <h3>Enjoys {cat.enjoys}.</h3>
         <NavLink to={`/catedit/${cat.id}`}>Edit this cat</NavLink>
+        <br></br>
+        <Button onClick={() => deleteCat()}>Delete this cat</Button>
       </div>
       }
     </>
